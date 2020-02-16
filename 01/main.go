976f6ce66ae094016f7df2d1802ebf3e04131edc
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-02-16 21:34:29 +0800

    finish 01

---
+++ 01/main.go
@@ -0,0 +1,23 @@
+func twoSum_perfect(nums []int, target int) []int {
+	index := make(map[int]int, len(nums))
+
+	for i, num := range nums {
+		if j, ok := index[target-num]; ok == true {
+			return []int{j, i}
+		}
+		index[num] = i
+	}
+
+	return []int{}
+}
+
+func twoSum(nums []int, target int) []int {
+    for i := 0; i < len(nums); i++ {
+        for j := i+1; j < len(nums); j++ {
+            if target - nums[i] == nums[j]{
+                return []int{i,j}
+            }
+        }
+    }
+    return []int{}
+}
