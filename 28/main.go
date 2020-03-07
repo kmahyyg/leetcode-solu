51ffb1c646ca44960ac1ba0f98494187d9a14820
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-03-07 20:59:37 +0800

    update 28
    Signed-off-by: kmahyyg <16604643+kmahyyg@users.noreply.github.com>

--- 28/main.go
+++ 28/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-
+	fmt.Println(strStrv1("hello", "ll"))
 }
 
-func strStr(haystack string, needle string) int {
+func strStrv1(haystack string, needle string) int {
+	// official builtin library, rabin-karp alg. internally with bytes
 	return strings.Index(haystack, needle)
 }
