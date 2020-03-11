45982c1d7fed86b4eab33bb7537006bb51c557dc
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-03-11 11:52:29 +0800

    finish 33

--- 33/main.go
+++ 33/main.go
@@ -6,30 +6,31 @@ func main() {
 	fmt.Println(search([]int{2, 1}, 2))
 	fmt.Println(search([]int{5, 1, 3}, 3))
 	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0))
+	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2, 3}, 2))
 	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 3))
 }
 
-// binary search here
-// if nums[left:mid] < nums[mid] < nums[mid:right], then
-// nums[left:mid] must be increasing only
 func search(nums []int, target int) int {
-	current, mid, last := 0, 0, len(nums)
-	for current != last {
-		mid = current + (last - current) / 2
-		if nums[mid] == target {
-			return mid
+	if len(nums) == 0 {
+		return -1
+	}
+	s, e, m := 0, len(nums)-1, 0
+	for s <= e {
+		m = (s + e) / 2
+		if target == nums[m] {
+			return m
 		}
-		if nums[current] <= nums[mid] {
-			if nums[current] <= target && target < nums[mid] {
-				last = mid
+		if nums[s] <= nums[m] {
+			if nums[m] > target && nums[s] <= target {
+				e = m - 1
 			} else {
-				current = mid + 1
+				s = m + 1
 			}
 		} else {
-			if nums[current] < target && target <= nums[last-1] {
-				current = mid + 1
+			if nums[m] < target && nums[e] >= target {
+				s = m + 1
 			} else {
-				last = mid
+				e = m - 1
 			}
 		}
 	}
